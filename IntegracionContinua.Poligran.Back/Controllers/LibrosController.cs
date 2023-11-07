7fb9edacc09a8cde92115c7e163901ae41daeeda
lian-ju <wjulianpineda@gmail.com>
2023-11-06 21:02:27 -0500

    Protección de archivos necesarios

---
+++ IntegracionContinua.Poligran.Back/Controllers/LibrosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntegracionContinua.Poligran.Back.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LibrosController : Controller
+    {
+     
+        private readonly ILogger<LibrosController> _logger;
+
+        public LibrosController(ILogger<LibrosController> logger)
+        {
+            _logger = logger;
+        }
+        // Simulación de una lista de libros
+    private List<Libro> libros = new List<Libro>
+    {
+        new Libro { Id = 1, Titulo = "El señor de los anillos", Autor = "J. R. R. Tolkien", AnioPublicacion = 1937 },
+        new Libro { Id = 2, Titulo = "Cien años de soledad", Autor = "Gabriel García Márquez", AnioPublicacion = 1967 },
+        new Libro { Id = 3, Titulo = "Orgullo y prejuicio", Autor = "Jane Austen", AnioPublicacion = 1813 }
+    };
+
+        [HttpGet]
+        public IEnumerable<Libro> Get()
+        {
+            return libros;
+        }
+    }
+}
---
+++ IntegracionContinua.Poligran.Back/Libro.cs
@@ -0,0 +1,10 @@
+﻿namespace IntegracionContinua.Poligran.Back
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnioPublicacion { get; set; }
+    }
+}
